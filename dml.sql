55d0a789e36500c61f85142ac6e54a23171c0d13
kunxin-chor <chorkunxin@yahoo.com>
2022-12-02 03:45:32 +0000

    Added DML

--- dml.sql
+++ dml.sql
@@ -10,3 +10,18 @@ insert into parents (first_name, last_name) values ("Zoe", "Tay"),
     ("Dick", "Lee"),
     ("Junjie", "Lin"),
     ("Wang", "Lei");
+
+insert into locations (name, address) values ("Yishun Swimming Complex", "351 Yishun Ave 3, Singapore 769057"),
+ ("Jurong West Swimming Complex","21 Jurong West Street 93, Singapore 648965"), 
+ ("Tampines Swimming Complex", "1 Tampines Walk, Singapore 528523"),
+  ("Choa Chu Kang Swimming Complex", "1 Choa Chu Kang Street 53, Singapore 689236");
+
+-- delete a row (the WHERE part is ultra important)
+-- if we omit th WHERE, it will delete all the rows from the table
+delete from locations where location_id=2;
+
+update parents set first_name="Lyon" where parent_id = 1;
+
+-- inserting a student
+insert into students (parent_id, first_name, last_name, date_of_birth, swimming_level)
+ values (5, "Richard", "Lee", "2018-06-02", 1);
